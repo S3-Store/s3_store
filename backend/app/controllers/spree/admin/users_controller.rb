c0f863babead57cece0b8f9706eba467d53331ea
Mayur Shah <shahmayur001@gmail.com>
2025-04-08 20:15:48 +0530

    Enforce User Group Assignment Upon Signup
    - Updated UsersController to assign the default user group to new users if the current store has the `enforce_group_upon_signup` setting enabled.
    - Added a checkbox in the store settings form to allow administrators to enable or disable the `enforce_group_upon_signup` feature.
    - Included a new translation key for the `enforce_group_upon_signup` label in the English locale file.
    
    This feature ensures that all new users are automatically added to a specified user group, improving user management and organization within the Spree application.

--- backend/app/controllers/spree/admin/users_controller.rb
+++ backend/app/controllers/spree/admin/users_controller.rb
@@ -25,6 +25,7 @@ module Spree
         if @user.save
           set_roles
           set_stock_locations
+          set_user_group if current_store&.enforce_group_upon_signup
 
           flash[:success] = t('spree.created_successfully')
           redirect_to edit_admin_user_url(@user)
@@ -155,6 +156,13 @@ module Spree
         if user_params[:stock_location_ids]
           @user.stock_locations =
             Spree::StockLocation.accessible_by(current_ability).where(id: user_params[:stock_location_ids])
+        end
+      end
+
+      def set_user_group
+        if @user && @user.user_group.nil?
+          user_group = current_store.default_cart_user_group
+          @user.update(user_group: user_group) if user_group
         end
       end
     end
