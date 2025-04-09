9de09205efb1b579e5f809b7def887f8c264dc0a
Mayur Shah <shahmayur001@gmail.com>
2025-04-09 15:50:17 +0530

    Add Price List Price Check for Variants and Prices
    Implemented `check_pricelist_price` in variant API and price admin controllers to prevent the user from updating prices which are associated to a price list.

--- backend/app/controllers/spree/admin/prices_controller.rb
+++ backend/app/controllers/spree/admin/prices_controller.rb
@@ -4,6 +4,7 @@ module Spree
   module Admin
     class PricesController < ResourceController
       belongs_to 'spree/product', find_by: :slug
+      before_action :check_pricelist_price, only: [:edit, :update, :destroy]
 
       def index
         params[:q] ||= {}
@@ -22,6 +23,19 @@ module Spree
       end
 
       def edit
+      end
+
+      private
+
+      # Redirects with an error message if the price has is_pricelist_price set to true.
+      # which indicates that the price is part of a pricelist. This is to prevent
+      # modification or deletion of prices that are part of a pricelist via the
+      # product price interface.
+      def check_pricelist_price
+        if @price.is_pricelist_price
+          flash[:error] = t('spree.admin.prices.errors.price_belongs_to_pricelist')
+          redirect_to admin_product_prices_path(@product)
+        end
       end
     end
   end
