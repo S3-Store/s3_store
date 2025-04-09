9de007a0599dc6e642bfe78ac105ab9967c286f5
Mayur Shah <shahmayur001@gmail.com>
2025-04-09 14:56:02 +0530

    Add price list info on user group admin
    This commits adds column on user group to display and select the associated price list
    for a user group.

--- backend/app/controllers/spree/admin/user_groups_controller.rb
+++ backend/app/controllers/spree/admin/user_groups_controller.rb
@@ -3,6 +3,13 @@
 module Spree
   module Admin
     class UserGroupsController < ResourceController
+      before_action :load_data, except: :index
+
+      private
+
+      def load_data
+        @price_list = Spree::PriceList.order(:name)
+      end
     end
   end
 end
