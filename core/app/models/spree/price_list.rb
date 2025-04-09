b8923ae499bc76014cad62479fc941f1fffc75f4
Mayur Shah <shahmayur001@gmail.com>
2025-04-09 14:41:40 +0530

    Add Price List and Price List Item Models
    - Introduced `Spree::PriceList` model to manage collections of prices associated with specific user groups and countries.
    - Added optional association for `price_list` on `user_group`
    - Created `Spree::PriceListItem` model to establish a many-to-many relationship between prices and price lists.
    - Updated `Spree::Price` model to include associations with price lists through price list items.
    - Added `contain_taxes` attribute to the `Spree::Price` model to indicate whether prices include taxes.
    - Implemented validations for the new models to ensure data integrity.
    - Added necessary database migrations to create price lists and price list items tables.
    
    These changes enhance the pricing structure by allowing for more flexible pricing strategies based on user groups and geographical considerations.

---
+++ core/app/models/spree/price_list.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Spree
+  class PriceList < Spree::Base
+    has_many :price_list_items, class_name: 'Spree::PriceListItem', dependent: :destroy
+    has_many :prices, through: :price_list_items, class_name: 'Spree::Price'
+    has_many :user_groups, class_name: 'Spree::UserGroup', dependent: :destroy
+    belongs_to :country, class_name: "Spree::Country", foreign_key: "country_iso", primary_key: "iso", optional: true
+
+    validates :currency, inclusion: { in: ::Money::Currency.all.map(&:iso_code), message: :invalid_code }
+    validates :country, presence: true, unless: -> { for_any_country? }
+
+    validates :name, presence: true
+
+    def for_any_country?
+      country_iso.nil?
+    end
+
+    def country_iso=(country_iso)
+      self[:country_iso] = country_iso.presence
+    end
+  end
+end
